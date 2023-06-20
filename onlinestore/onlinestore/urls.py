96b645b69678ecabf06916c4fb79c87b32ec9f19
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-06-20 00:30:13 -0300

    core app done

--- onlinestore/onlinestore/urls.py
+++ onlinestore/onlinestore/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, contact
 
 urlpatterns = [
     path('', index , name='index'),
+    path('contact/',contact, name='contact'),
     path('admin/', admin.site.urls), 
 ]
